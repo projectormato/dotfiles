d2f2da7633b63bfb7d41592a89aebe6500c868a6
projectormato <k.keita811.anuy@gmail.com>
2020-04-09 22:21:20 +0900

    add: cppinitのMac版を追加

---
+++ cppinitForMac/main.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define step(i, s, n, d) for(int i=s; i<n; i+=d)
+#define FOR(i,s,n) step(i,s,n,1)
+#define rep(i,n) FOR(i,0,n)
+#define ll long long
+typedef pair<int, int> P;
+
+int main(){
+}
