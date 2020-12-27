595c5f27c1242623494ef564cec0cc048bf8a093
projectormato <k.keita811.anuy@gmail.com>
2020-12-27 23:58:07 +0900

    modify: cppinit加筆

--- cppinitForMac/main.cpp
+++ cppinitForMac/main.cpp
@@ -3,8 +3,10 @@ using namespace std;
 #define step(i, s, n, d) for(int i=s; i<n; i+=d)
 #define FOR(i,s,n) step(i,s,n,1)
 #define rep(i,n) FOR(i,0,n)
+#define forEntry(entries) for(auto entry : entries)
 #define ll long long
 typedef pair<int, int> P;
+typedef map<int,int> M;
 
 int main(){
 }
